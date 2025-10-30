231af53c0bf7bfdb5cf0e618c5c25fc77b131c17
Stefinie Fernando <minolispencer@gmail.com>
2025-10-30 11:01:48 +0530

    Fix prettier issues

--- plugins/platform-engineer-core/src/components/PlatformDetailsCard/EmptyDataplanesState.tsx
+++ plugins/platform-engineer-core/src/components/PlatformDetailsCard/EmptyDataplanesState.tsx
@@ -20,4 +20,3 @@ export const EmptyDataplanesState: React.FC = () => {
     </Card>
   );
 };
-
--- plugins/platform-engineer-core/src/components/PlatformDetailsCard/EnvironmentsGrid.tsx
+++ plugins/platform-engineer-core/src/components/PlatformDetailsCard/EnvironmentsGrid.tsx
@@ -39,4 +39,3 @@ export const EnvironmentsGrid: React.FC<EnvironmentsGridProps> = ({
     </Box>
   );
 };
-
