4511a2d8e27d40c20acfa38ebf1a187d615bd9ac
Stefinie Fernando <minolispencer@gmail.com>
2025-11-05 13:15:07 +0530

    Add user-settings backend plugin for centralized storage of user preferences

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -54,6 +54,9 @@ backend.add(import('@backstage/plugin-search-backend-module-pg'));
 backend.add(import('@backstage/plugin-search-backend-module-catalog'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 
+// user settings plugin - enables centralized storage for starred entities and user preferences
+backend.add(import('@backstage/plugin-user-settings-backend'));
+
 backend.add(import('@openchoreo/backstage-plugin-backend'));
 backend.add(import('@openchoreo/backstage-plugin-catalog-backend-module'));
 backend.add(import('@openchoreo/backstage-plugin-scaffolder-backend-module'));
