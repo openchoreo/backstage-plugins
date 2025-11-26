b7661cee600b7d244dc72bcb4020c577a9acd46a
Kavith Lokuhewage <kaviththiranga@gmail.com>
2025-11-27 04:59:24 +0530

    Remove unused code after revamping popup pages

--- plugins/openchoreo/src/components/Environments/ReleaseDataRenderer/index.ts
+++ plugins/openchoreo/src/components/Environments/ReleaseDataRenderer/index.ts
@@ -1,3 +1,2 @@
-export { ReleaseDataRenderer } from './ReleaseDataRenderer';
 export { ReleaseInfoTabbedView } from './ReleaseInfoTabbedView';
 export type { ReleaseData } from './types';
--- plugins/openchoreo/src/components/Environments/ReleaseDataRenderer/utils.ts
+++ plugins/openchoreo/src/components/Environments/ReleaseDataRenderer/utils.ts
@@ -1,27 +1,3 @@
-import { StyleClasses } from './styles';
-import { HealthStatus } from './types';
-
-/**
- * Get the appropriate chip class based on health status.
- */
-export function getHealthChipClass(
-  healthStatus: HealthStatus | undefined,
-  classes: StyleClasses,
-): string {
-  switch (healthStatus) {
-    case 'Healthy':
-      return classes.healthyChip;
-    case 'Progressing':
-      return classes.progressingChip;
-    case 'Degraded':
-      return classes.degradedChip;
-    case 'Suspended':
-      return classes.suspendedChip;
-    default:
-      return classes.unknownChip;
-  }
-}
-
 /**
  * Format an ISO timestamp to a locale string.
  */
