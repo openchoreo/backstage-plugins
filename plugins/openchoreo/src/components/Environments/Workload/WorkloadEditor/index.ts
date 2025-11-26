a009bb69d56309be3522a4217115accd54a7d5fe
Kavith Lokuhewage <kaviththiranga@gmail.com>
2025-11-27 04:59:24 +0530

    Remove unused code after revamping popup pages

--- plugins/openchoreo/src/components/Environments/Workload/WorkloadEditor/index.ts
+++ plugins/openchoreo/src/components/Environments/Workload/WorkloadEditor/index.ts
@@ -1,7 +1,4 @@
 export { WorkloadEditor } from './WorkloadEditor';
-export { ContainerSection } from './ContainerSection';
-export { EndpointSection } from './EndpointSection';
-export { ConnectionSection } from './ConnectionSection';
 export { ContainerContent } from './ContainerContent';
 export { EndpointContent } from './EndpointContent';
 export { ConnectionContent } from './ConnectionContent';
