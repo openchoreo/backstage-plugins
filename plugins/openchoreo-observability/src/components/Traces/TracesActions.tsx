7a36951161bc7948dea6da8a8ef7be0df7275da8
Akila-I <akila.99g@gmail.com>
2025-12-07 20:49:09 +0530

    Add callbacks fro trace fetching
    - Updated ObservabilityTracesPage to use useCallback for handleFiltersChange and handleRefresh for improved performance.
    - Refactored TracesActions to be a memoized component, optimizing rendering and preventing unnecessary updates.

--- plugins/openchoreo-observability/src/components/Traces/TracesActions.tsx
+++ plugins/openchoreo-observability/src/components/Traces/TracesActions.tsx
@@ -1,6 +1,7 @@
 import { Box, Typography, Button } from '@material-ui/core';
 import RefreshIcon from '@material-ui/icons/Refresh';
 import { useTracesActionsStyles } from './styles';
+import { memo } from 'react';
 
 interface TracesActionsProps {
   totalCount: number;
@@ -8,34 +9,34 @@ interface TracesActionsProps {
   onRefresh: () => void;
 }
 
-export const TracesActions = ({
-  totalCount,
-  disabled,
-  onRefresh,
-}: TracesActionsProps) => {
-  const classes = useTracesActionsStyles();
+export const TracesActions = memo(
+  ({ totalCount, disabled, onRefresh }: TracesActionsProps) => {
+    const classes = useTracesActionsStyles();
 
-  return (
-    <Box className={classes.statsContainer}>
-      <Box>
-        <Typography variant="body2" color="textSecondary">
-          Total traces: {totalCount}
-        </Typography>
-        <Typography variant="body2" color="textSecondary">
-          Last updated at: {new Date().toLocaleString()}
-        </Typography>
+    return (
+      <Box className={classes.statsContainer}>
+        <Box>
+          <Typography variant="body2" color="textSecondary">
+            Total traces: {totalCount}
+          </Typography>
+          <Typography variant="body2" color="textSecondary">
+            Last updated at: {new Date().toLocaleString()}
+          </Typography>
+        </Box>
+        <Box className={classes.actionsContainer}>
+          <Button
+            variant="outlined"
+            startIcon={<RefreshIcon />}
+            onClick={onRefresh}
+            disabled={disabled}
+          >
+            Refresh
+          </Button>
+          {/* TODO: Add Auto Refresh Button, Sort Button and Download Button */}
+        </Box>
       </Box>
-      <Box className={classes.actionsContainer}>
-        <Button
-          variant="outlined"
-          startIcon={<RefreshIcon />}
-          onClick={onRefresh}
-          disabled={disabled}
-        >
-          Refresh
-        </Button>
-        {/* TODO: Add Auto Refresh Button, Sort Button and Download Button */}
-      </Box>
-    </Box>
-  );
-};
+    );
+  },
+);
+
+TracesActions.displayName = 'TracesActions';
