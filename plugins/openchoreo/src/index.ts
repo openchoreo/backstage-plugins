b0db6f1a8aec2169c41b89400fe3f1ca90289ad2
Kavith Lokuhewage <kaviththiranga@gmail.com>
2025-12-19 02:41:35 +0530

    Remove deprecated Build views codebase

--- plugins/openchoreo/src/index.ts
+++ plugins/openchoreo/src/index.ts
@@ -1,7 +1,6 @@
 export { choreoPlugin } from './plugin';
 export { Environments } from './components/Environments/Environments';
 export { CellDiagram } from './components/CellDiagram/CellDiagram';
-export { Builds } from './components/Builds/Builds';
 export {
   RuntimeLogs,
   Workflows,
