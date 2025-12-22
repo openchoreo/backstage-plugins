dce72c0b389982f28931925392c3545d62ef7eb5
Stefinie Fernando <minolispencer@gmail.com>
2025-12-22 11:16:27 +0530

    Update scaffolder to use proper auth feature flag

--- plugins/scaffolder-backend-module-openchoreo/src/actions/project.ts
+++ plugins/scaffolder-backend-module-openchoreo/src/actions/project.ts
@@ -60,7 +60,7 @@ export const createProjectAction = (config: Config) => {
 
       // Check if authorization is enabled (defaults to true)
       const authzEnabled =
-        config.getOptionalBoolean('openchoreo.features.authz.enabled') ?? true;
+        config.getOptionalBoolean('openchoreo.features.auth.enabled') ?? true;
 
       // Get user token from secrets (injected by form decorator) when authz is enabled
       const token = authzEnabled
