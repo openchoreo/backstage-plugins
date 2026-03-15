0b7a6ce0e19c347afe3074b1adf8d846595fca49
yashodgayashan <yashodgayashan@gmail.com>
2026-03-15 16:03:42 +0530

    fix: cluster component type create error
    Signed-off-by: yashodgayashan <yashodgayashan@gmail.com>

--- packages/app/src/scaffolder/ComponentTypeYamlEditor/ComponentTypeYamlEditorExtension.tsx
+++ packages/app/src/scaffolder/ComponentTypeYamlEditor/ComponentTypeYamlEditorExtension.tsx
@@ -18,10 +18,18 @@ const DEFAULT_COMPONENT_TYPE_TEMPLATE = {
   },
   spec: {
     workloadType: 'deployment',
-    schema: {
-      parameters: {},
-      environmentConfigs: {
-        replicas: 'integer | default=1',
+    parameters: {
+      openAPIV3Schema: {},
+    },
+    environmentConfigs: {
+      openAPIV3Schema: {
+        type: 'object',
+        properties: {
+          replicas: {
+            type: 'integer',
+            default: 1,
+          },
+        },
       },
     },
     resources: [
