e633e2879b8436ccfb86a66c72231c3cef18c715
Kavith Lokuhewage <kaviththiranga@gmail.com>
2026-05-26 14:21:09 +0530

    fix: license headers
    Signed-off-by: Kavith Lokuhewage <kaviththiranga@gmail.com>

--- packages/app/e2e-tests/a11y.spec.ts
+++ packages/app/e2e-tests/a11y.spec.ts
@@ -1,13 +1,3 @@
-/*
- * Copyright 2024 The Backstage Authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- */
-
 import { test, expect } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 
